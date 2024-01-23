bfcb000945b19819f2e1f54cd852070d3534a368
Maxamed Maxamed <20088349@mail.wit.ie>
2024-01-23 14:22:19 +0000

    new app created

---
+++ app/src/main/java/com/example/fyp_hydrate_main_app/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.fyp_hydrate_main_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // TODO: This method is called when the activity is first created.
+        // Make sure to call finish() or else the activity will be leaked!
+
+
+
+    }
+
+    override fun onResume() {
+    super.onResume()
+    // TODO: This method is called when the activity may be visible to the user.
+    // Make sure to call finish() or else the activity will be leaked!
+
+
+}
+
+
+}
